8f252bd864cc9f080182d1075bd83bfa1dd365a1
rafaelvieiramaximo <rafavmaximo@gmail.com>
2025-05-08 10:56:35 -0300

    Refactor HomeScreen component to implement binary conversion functionality and update UI elements

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,84 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
+
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+  
+  const [number, setInputValue] = React.useState('');
+  const [bin, setBin] = React.useState('');
 
+  function convertBinary(number: string) {
+
+    let numbers = parseInt(number);
+    let bin = 0;
+    let rem, i = 1;
+    while (numbers !== 0) {
+      rem = numbers % 2;
+      numbers = Math.floor(numbers / 2);
+      bin = bin + rem * i;
+      i = i * 10;
+    }
+    return bin;
+  }
+
+  
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.title}>Conversor</Text>
+      <Text style={styles.subtitle}>Insira o número aqui:</Text>
+      <TextInput 
+        keyboardType='numeric'
+        placeholder='Digite a ser convertido'
+        style={styles.inputenter}
+        value={number}
+        onChangeText={convertBinary}
+        ></TextInput>
+        <TextInput
+        onChangeText={(number) => convertBinary(number)}
+        value={bin}
+        style={styles.input}
+        ></TextInput>
+    </View>
+
+)
+
+} 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
-    gap: 8,
+    gap: 30,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title: {
+    fontSize: 40,
+    marginTop: 50,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  subtitle: {
+    fontSize: 20,
   },
-});
+  inputenter:{
+    width: 300,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 20,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  input:{
+    width: 300,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 20,
+    marginTop: 20,
+  }
+})
